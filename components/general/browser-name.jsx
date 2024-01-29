ad0ad619e4e3f912b487916eb9e1433a5e591efa
cocoth <anonymousid0000000@gmail.com>
2024-01-29 10:21:53 +0700

    modified:   components/general/browser-name.jsx

--- components/general/browser-name.jsx
+++ components/general/browser-name.jsx
@@ -6,34 +6,34 @@ import {
   BsQuestion,
 } from "react-icons/bs";
 import { FaSafari } from "react-icons/fa";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 
 const BrowserName = () => {
+  const [icon, setIcon] = useState(null)
+
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const ua = navigator.userAgent.toLowerCase()
-    }
-  },[])
-  try {
     const ua = navigator.userAgent.toLowerCase()
     if (typeof window !== 'undefined') {
       const ua = navigator.userAgent.toLowerCase()
     }
-    const browser = (
-      ua.includes('firefox') ? (<BsBrowserFirefox />) :
-        ua.includes('chrome') ? (<BsBrowserChrome />) :
-          // ua.includes('chrome') ? ( <img src={chromeIcon} alt="Chrome Icon" width={1000} height={1000} 
-          // className='w-7 h-auto' />):
-          ua.includes('edg') ? (<BsBrowserEdge />) :
-            ua.includes('safari') ? (<FaSafari />) :
-              (<BsQuestion />)
-    )
-    return browser
-  } catch (error) {
-    throw (error)
-  }
+    try {
+      const browser = (
+        ua.includes('firefox') ? (<BsBrowserFirefox />) :
+          ua.includes('chrome') ? (<BsBrowserChrome />) :
+            // ua.includes('chrome') ? ( <img src={chromeIcon} alt="Chrome Icon" width={1000} height={1000} 
+            // className='w-7 h-auto' />):
+            ua.includes('edg') ? (<BsBrowserEdge />) :
+              ua.includes('safari') ? (<FaSafari />) :
+                (<BsQuestion />)
+      )
+      setIcon(browser)
+    } catch (error) {
+      throw (error)
+    }
+  }, [])
+  return icon
 }
 
-  export default BrowserName
+export default BrowserName
